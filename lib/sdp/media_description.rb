6176793160a24793bdec232f17a17c4910d2094d
Christian Rusch <christian.rusch@wingo.ch>
2013-03-21 13:25:14 +0100

    Fix SDP::MediaDescription. It was unfinished.

--- lib/sdp/media_description.rb
+++ lib/sdp/media_description.rb
@@ -3,38 +3,67 @@ require_relative 'runtime_error'
 class SDP
   class MediaDescription
     ALLOWED_TYPES = [
-      :video, :audio, :text, :application, :message, :control, :data
+      'video', 'audio', 'text', 'application', 'message', 'control', 'data'
     ]
 
-    def initialize(type)
-      @type = type
+    FIELDS = [
+      :media,
+      :transport_port,
+      :transport_protocol,
+      :transport_format,
+      :information,
+      :bandwidths,
+      :connection_network_type,
+      :connection_address_type,
+      :connection_address,
+      :attributes,
+    ]
+
+    FIELDS.each do |field|
+      attr_accessor field
     end
 
-    def media=(type)
-      unless ALLOWED_TYPES.include? type
+    def initialize(hash = {})
+      FIELDS.each do |field|
+        send("#{field}=", hash[field]) if hash.has_key?(field)
+      end
+
+      # Deprecate these?
+      [ :port, :protocol, :format ].each do |field|
+        send("transport_#{field}=", hash[field]) if hash.has_key?(field)
+      end
+    end
+
+    def media=(value)
+      unless ALLOWED_TYPES.include?(value)
         raise SDP::RuntimeError,
-          "Media type '#{type}' must be in list #{ALLOWED_TYPES.join(', ')}"
+          "Media type #{value.inspect} not one of #{ALLOWED_TYPES.join(', ')}"
       end
 
-      @media = type
-    end
-
-    # The transport port to which the media stream is sent.
-    def transport_port=(port)
-      if port.match /\//
-
-      end
-    end
-
-    def transport_protocol=(protocol)
-
+      @media = value
     end
 
     def to_s
-      session = "m=#{m[:media]} #{m[:port]} #{m[:protocol]} #{m[:format]}\r\n"
+      session = ("m=%s %s %s %s\r\n" %
+        [ media, transport_port, transport_protocol, transport_format ])
 
-      if m[:attributes]
-        m[:attributes].each do |a|
+      if information
+        session << ("i=%s\r\n" % [ information ])
+      end
+
+      if connection_network_type
+        session << ("c=%s %s %s\r\n" %
+          [ connection_network_type, connection_address_type, connection_address ])
+      end
+
+      if bandwidths
+        bandwidths.each do |b|
+          session << ("b=%s:%s\r\n" % [ b[:bandwidth_type], b[:bandwidth] ])
+        end
+      end
+
+      if attributes
+        attributes.each do |a|
           session << "a=#{a[:attribute]}"
           session << ":#{a[:value]}" if a[:value]
           session << "\r\n"
